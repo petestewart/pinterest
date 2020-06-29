1287b717fb34340e9eaf3b0d84110c819580740f
Pete Stewart <petestew@gmail.com>
2020-06-28 20:04:00 -0500

    now displaying unstylized pins

--- src/javascripts/components/pinList/pinList.js
+++ src/javascripts/components/pinList/pinList.js
@@ -1,11 +1,11 @@
 import pinData from '../../helpers/data/pinData';
 import utils from '../../helpers/utils';
+import './pinList.scss';
 
 const showBoard = (e) => {
   const boardId = e.currentTarget.id;
   pinData.getBoardPins(boardId)
     .then((pins) => {
-      console.error(pins[0].url);
       utils.printToDom('#header', `<h2 class="text-center">${boardId}</h2>`);
       let domString = '';
       pins.forEach((pin) => {
